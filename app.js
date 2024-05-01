c02a0ad9f6c2f892b73c89112d012f19943d054a
bhaveshrb <bhavesh.fluxbyte@gmail.com>
2024-05-01 10:22:56 +0530

    live testing

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 // const client = require('./database'); 
-import db from './app/config/database.js';
 // const express = require('express');
 import Express from 'express';
-import UsersRoutes from './app/routes/users.route.js';
-import AuthRoutes from './app/routes/auth.route.js';
 import { config } from './app/config/index.js';
 const app = Express();
  
@@ -13,7 +10,7 @@ app.listen(config.port,()=>{
 })
 
 //console.log(client);
-db.client.connect();
+// db.client.connect();
 
 // app.get('/stud',(req,res)=>{
 //   client.query(`select * from stud`,(err,result)=>{
@@ -24,18 +21,6 @@ db.client.connect();
 //   });
 //   client.end;
 // })
-app.get('/stud/:id',(req,res)=>{
-  db.client.query(`select * from stud where id = ${req.params.id}`,(err,result)=>{
-    if(!err){
-      res.send(result.rows);
-    }
-  });
-  
-})
-app.use('/auth',AuthRoutes);
-app.use('/stud/users',UsersRoutes);
-
-
 // {
 //   "name": "khhj",
 //   id: 8
